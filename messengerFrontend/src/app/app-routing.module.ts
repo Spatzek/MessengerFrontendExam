860300c970083b5e18ac02e6eeae9677613223b1
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2021-05-24 13:52:02 +0200

    INSERT

---
+++ messengerFrontend/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes =
+  [{ path: 'chats', loadChildren: () => import('./chats/chats.module').then(m => m.ChatsModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
